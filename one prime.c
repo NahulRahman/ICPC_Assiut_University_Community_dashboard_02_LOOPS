6c63e1b0ac39bf0e5283d00f7f0034f4f90ec84a
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-02-26 11:53:08 -0800

    Add files via upload

---
+++ one prime.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int i, num, isPrime;
+    isPrime = 1;
+    scanf("%d", &num);
+    for(i=2; i<=num/2; i++){
+        if(num%i==0){
+            isPrime = 0;
+            break;
+        }
+    }
+    if(isPrime == 1 && num > 1) printf("YES");
+    else printf("NO");
+    return 0;
+}
