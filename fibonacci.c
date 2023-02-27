26d0f7061023fc84ba99a486adb47e2c7c3b7c1f
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-02-27 03:32:40 -0800

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a=0,b=1,c=0,n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printf("%d ",c);
+        a=b;
+        b=c;
+        c=a+b;
+    }return 0;
+}
